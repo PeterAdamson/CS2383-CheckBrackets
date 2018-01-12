0bd20fbc7a9bbc6f50bacdbee3d04ec205b2b5bd
PeterAdamson <adamsonpeterj@gmail.com>
2018-01-12 12:44:20 -0400

    Create FullStackException.java

---
+++ FullStackException.java
@@ -0,0 +1,13 @@
+//Author Peter Adamson
+
+public class FullStackException extends Exception
+{
+	public FullStackException()
+	{
+	}
+	
+	public FullStackException(String message)
+	{
+		super(message);
+	}
+}
