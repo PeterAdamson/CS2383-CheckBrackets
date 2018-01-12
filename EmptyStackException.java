03d4fb10925ca37f307a2acc4e7982be0eb09152
PeterAdamson <adamsonpeterj@gmail.com>
2018-01-12 12:44:01 -0400

    Create EmptyStackException.java

---
+++ EmptyStackException.java
@@ -0,0 +1,13 @@
+//Author Peter Adamson 3319005
+
+public class EmptyStackException extends Exception
+{
+	public EmptyStackException()
+	{
+	}
+	
+	public EmptyStackException(String message)
+	{
+		super(message);
+	}
+}
